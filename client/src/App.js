d8d1322a30b5e0a033f4e6d510e581a77c240773
Clarissa Ardon <klary.ardon@gmail.com>
2021-12-30 21:19:54 +0100

    C:/Program Files/Git/phones route added

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-      fetch("/api")
+      fetch("/phones")
         .then((res) => res.json())
         .then((data) => setData(data.message));
     }, []);
--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 // server/index.js
-
+const path = require('path');
 const express = require("express");
 
 const PORT =  process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/phones", (req, res) => {
+    res.json({ message: "Hello from server, soon we will see some phones!" });
 })
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 })
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
