611e6b31666178e4fa832c65fb00d09f3f6b12f6
Clarissa Ardon <klary.ardon@gmail.com>
2021-12-31 13:01:06 +0100

    cors added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 // server/index.js
 const path = require('path');
 const express = require("express");
+const cors = require('cors');
 const dbPhones = require("../db.json") 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
+
+// Allows CORS for all resources on your server.
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
