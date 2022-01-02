81c73b265b58c41e96c218d5830230709a633209
Clarissa Ardon <klary.ardon@gmail.com>
2022-01-02 20:43:19 +0100

    modal created and styled

--- client/src/components/PhoneCard/PhoneCard.js
+++ client/src/components/PhoneCard/PhoneCard.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Styles.module.css";
-// import formatterNumber from "../../helpers/utils";
+import formatterNumber from "../../helpers/utils";
 
 const PhoneCard = ({ item }) => {
+  const [showModal, setShowModal] = useState(true);
+
+  const toggleModal = (selectedItem) => {
+    setShowModal(!showModal);
+  };
+
   const {
     name,
     manufacturer,
@@ -14,19 +20,40 @@ const PhoneCard = ({ item }) => {
     processor,
     ram,
   } = item;
+
   return (
     <div className={styles.phoneCardContainer}>
       <div className={styles.phoneName}>{name}</div>
       <div className={styles.imageContainer}>
-        <img src={imageFileName} alt={name} className={styles.image} />
+        <img
+          src={imageFileName}
+          alt={name}
+          className={styles.image}
+          onClick={() => toggleModal(item)}
+        />
       </div>
-      {/* <div>{manufacturer}</div>
-      <div>{description}</div>
-      <div>{color}</div>
-      <div>{price}</div>
-      <div>{screen}</div>
-      <div>{processor}</div>
-      <div>{ram}</div> */}
+
+      {!showModal ? (
+        <div className={styles.modal}>
+          <div className={styles.titleAlignment}>
+            <div className={styles.modalTitle}>{name}</div>
+            <div className={styles.xModal} onClick={toggleModal}>
+              X
+            </div>
+          </div>
+          <div className={styles.modalText}>{manufacturer}</div>
+          <div className={styles.modalDescription}>{description}</div>
+          <div className={styles.modalText}>Color: {color}</div>
+          <div className={styles.modalText}>
+            Price: {formatterNumber(price)}
+          </div>
+          <div className={styles.modalText}>Screen: {screen}</div>
+          <div className={styles.modalText}>Processor: {processor}</div>
+          <div className={styles.modalText}>Ram: {ram}</div>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -3,16 +3,16 @@ import styles from "./Styles.module.css";
 import PhoneCard from "../components/PhoneCard/PhoneCard";
 
 const Home = ({ items }) => {
-    const phoneItem = items.map((item) => (
-        <PhoneCard key={item.id} item={item} />
-    ));
+  const phoneItem = items.map((item) => (
+    <PhoneCard key={item.id} item={item} />
+  ));
 
-    return (
-        <div className={styles.homeContainer}>
-            <div className={styles.title}>Phones Catalog</div>
-            <div className={styles.gallery}>{phoneItem}</div>
-        </div>
-    );
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.title}>Phones Catalog</div>
+      <div className={styles.gallery}>{phoneItem}</div>
+    </div>
+  );
 };
 
 export default Home;
