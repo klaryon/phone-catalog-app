7f3a9a28c1a496749f9912df4ab0cce2b8114194
Clarissa Ardon <klary.ardon@gmail.com>
2022-01-03 00:55:40 +0100

    react-loader-spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from "react";
+import Loader from "react-loader-spinner";
 import Home from "./screens/Home";
 
 function App() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch("/phones")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data))
+      .then(() => setLoading(false));
   }, []);
 
-  console.log(data);
-
-  return <div>{!data ? "Loading..." : <Home items={data.data} />}</div>;
+  if (loading) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Loader type="BallTriangle" color="#1b3a4b" height={150} width={150} />
+      </div>
+    );
+  } else {
+    return <div>{<Home items={data.data} />}</div>;
+  }
 }
 
 export default App;
